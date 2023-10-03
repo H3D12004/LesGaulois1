45753f168bf1ca086d378234d53398d6ebf3b528
H3D12004 <hedi.ben-jeddou@univ-tlse3.fr>
2023-10-03 09:02:08 +0200

    TP2

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,9 +3,12 @@ package personnages;
 public class Romain {
     private String nom;
     private int force;
+    private Equipement[] equipements = new Equipement[2];
+    private int nbEquipements = 0;
 
     public Romain(String nom, int force) {
         this.nom = nom;
+        assert force > 0 : "La force d'un Romain doit être positive";
         this.force = force;
     }
 
@@ -22,31 +25,68 @@ public class Romain {
     }
 
     public void recevoirCoup(int forceCoup) {
+        assert force > 0 : "Précondition violée: la force d'un Romain doit être positive avant de recevoir un coup";
+
+        int initialForce = force;
         force -= forceCoup;
+
         if (force > 0) {
             parler("Aïe");
         } else {
             parler("J'abandonne...");
+            force = 0;
+        }
+
+        assert force < initialForce : "Postcondition violée: la force d'un Romain doit diminuer après avoir reçu un coup";
+    }
+
+    public void sEquiper(Equipement equipement) {
+        switch (nbEquipements) {
+            case 0:
+            case 1:
+                if (nbEquipements == 1 && equipements[0] == equipement) {
+                    afficherMessage("possède déjà un", equipement.toString());
+                } else {
+                    equipements[nbEquipements++] = equipement;
+                    afficherMessage("s'équipe avec un", equipement.toString());
+                }
+                break;
+            case 2:
+                afficherMessage("est déjà bien protégé !", "");
+                break;
         }
     }
-    
+
+    private void afficherMessage(String action, String equipement) {
+        System.out.println("Le soldat " + nom + " " + action + (equipement.isEmpty() ? "" : " " + equipement) + ".");
+    }
+
     @Override
     public String toString() {
-    	return "Romain [nom = " + nom + ", force = " + force + "]";
+        return "Romain [nom = " + nom + ", force = " + force + "]";
     }
-    
+
     public static void main(String[] args) {
         Romain caesar = new Romain("César", 10);
         System.out.println(caesar);
 
-        // Tester la méthode parler (qui utilise prendreParole en interne)
         caesar.parler("Ave César");
 
-        // Tester la méthode recevoirCoup
-        caesar.recevoirCoup(3); // César devrait dire "Aïe" et sa force devrait être réduite
-        System.out.println(caesar); // Afficher l'état de César après avoir reçu le coup
+        caesar.recevoirCoup(3);
+        System.out.println(caesar);
 
-        caesar.recevoirCoup(8); // César devrait dire "J'abandonne..." car sa force sera négative ou zéro
-        System.out.println(caesar); // Afficher l'état de César après avoir reçu le deuxième coup
+        caesar.recevoirCoup(8);
+        System.out.println(caesar);
+
+        Romain minus = new Romain("Minus", 6);
+        System.out.println(minus);
+
+        System.out.println(Equipement.CASQUE);
+        System.out.println(Equipement.BOUCLIER);
+
+        minus.sEquiper(Equipement.CASQUE); // Le soldat Minus s'équipe avec un casque.
+        minus.sEquiper(Equipement.CASQUE); // Le soldat Minus possède déjà un casque.
+        minus.sEquiper(Equipement.BOUCLIER); // Le soldat Minus s'équipe avec un bouclier.
+        minus.sEquiper(Equipement.CASQUE); // Le soldat Minus est déjà bien protégé !
     }
 }
--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -2,12 +2,70 @@ package personnages;
 
 public class Village {
     private String nom;
+    private Chef chef;
+    private Gaulois[] villageois;
+    private int nbVillageois;
 
-    public Village(String nom) {
+    public Village(String nom, int nbVillageoisMaximum) {
         this.nom = nom;
+        this.villageois = new Gaulois[nbVillageoisMaximum];
+        this.nbVillageois = 0;
+    }
+
+    public void setChef(Chef chef) {
+        this.chef = chef;
     }
 
     public String getNom() {
         return nom;
+    }
+    
+
+    public void ajouterHabitant(Gaulois gaulois) {
+        if (nbVillageois < this.villageois.length) {
+            this.villageois[nbVillageois] = gaulois;
+            nbVillageois++;
+            System.out.println(gaulois.getNom() + " a rejoint le village " + nom + ".");
+        } else {
+            System.out.println("Le village est plein. " + gaulois.getNom() + " ne peut pas rejoindre le village.");
+        }
+    }
+
+    public Gaulois trouverHabitant(int numero) {
+        if (numero >= 0 && numero < nbVillageois) {
+            return villageois[numero];
+        } else {
+            System.out.println("Il n'y a pas de villageois avec le numéro " + numero + " dans le village.");
+            return null;
+        }
+    }
+
+    public void afficherVillageois() {
+        if (chef != null) {
+            System.out.println("Dans le village du chef " + chef.getNom() + " vivent les légendaires gaulois :");
+        } else {
+            System.out.println("Dans ce village sans chef vivent les légendaires gaulois :");
+        }
+
+        for (int i = 0; i < nbVillageois; i++) {
+            if (villageois[i] != null) {
+                System.out.println("- " + villageois[i].getNom());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Village village = new Village("Village des Irréductibles", 30);
+
+        Chef abraracourcix = new Chef("Abraracourcix", 6, village);
+        village.setChef(abraracourcix);
+
+        Gaulois asterix = new Gaulois("Astérix", 8);
+        village.ajouterHabitant(asterix);
+
+        Gaulois obelix = new Gaulois("Obélix", 25);
+        village.ajouterHabitant(obelix);
+
+        village.afficherVillageois();
     }
 }
